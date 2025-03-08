15629dd6e9a507b19f8269f29569cb28065cb0c8
Donatas Šauklys <donatas.sauklys@gmail.com>
2025-03-09 00:21:59 +0200

    Add authentication routes and AuthController for user registration and login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $fields = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        // COULD BE UNSAFE, IDK
+        $user = User::create($fields);
+        $token = $user->createToken($request->name);
+        return 
+        [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+    }
+
+    
+
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string',
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !password_verify($request->password, $user->password)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+        $token = $user->createToken($user->name);
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
+    }
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RadioStationController;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
 Route::apiResource("stations", RadioStationController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
